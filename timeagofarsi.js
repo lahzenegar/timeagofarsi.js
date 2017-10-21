dca979a2162ffadf3bb6c6069164e2467d5c3e3f
Fazrad Karkhani <farzad.karkhani@gmail.com>
2017-10-21 07:44:03 +0330

    Added future date

--- timeagofarsi.js
+++ timeagofarsi.js
@@ -5,43 +5,50 @@
  */
 
 export function timeAgoFarsi (startDate) {
+  
   if (!Date.now) {
     Date.now = function() { 
       return new Date().getTime(); 
     }
   }
 
-  var delta = Math.floor(Date.now() / 1000) - startDate;
+  var delta = Math.abs( Math.floor(Date.now() / 1000) - startDate ); 
+  var upcomingMode = false;
+
+  if (startDate > Math.floor(Date.now() / 1000)) {
+    upcomingMode = true;
+  }
+  
   var minute = 60,
       hour = minute * 60,
       day = hour * 24,
       month = day * 30,
-      year = day * 365;
-
+      year = day * 365,
+      postfix = upcomingMode ? 'بعد' : 'پیش'
 
   if (delta < 30) {
     return 'همین الان';
   } else if (delta < minute) {
-    return delta + ' ثانیه پیش';
+    return delta + ' ثانیه ' + postfix;
   } else if (delta < 2 * minute) {
-    return 'یک دقیقه پیش'
+    return 'یک دقیقه ' + postfix;
   } else if (delta < hour) {
-    return Math.floor(delta / minute) + ' دقیقه پیش';
+    return Math.floor(delta / minute) + ' دقیقه ' + postfix;
   } else if (Math.floor(delta / hour) == 1) {
-    return 'یک ساعت پیش'
+    return 'یک ساعت ' + postfix;
   } else if (delta < day) {
-    return Math.floor(delta / hour) + ' ساعت پیش';
+    return Math.floor(delta / hour) + ' ساعت ' + postfix;
   } else if (delta < day * 2) {
-    return 'دیروز';
+    return upcomingMode ? 'فردا' : 'دیروز';
   } else if (delta < month) {
-    return Math.floor(delta / day) + ' روز پیش';
+    return Math.floor(delta / day) + ' روز ' + postfix;
   } else if (Math.floor(delta / month) == 1) {
-    return 'یک ماه پیش';
+    return 'یک ماه ' + postfix;
   } else if (delta < year) {
-    return Math.floor(delta / month) + ' ماه پیش';
+    return Math.floor(delta / month) + ' ماه ' + postfix;
   } else if (Math.floor(delta / year) == 1) {
-    return 'یک سال پیش';
+    return 'یک سال ' + postfix;
   } else {
-    return Math.floor(delta / year) + ' سال پیش';
+    return Math.floor(delta / year) + ' سال ' + postfix;
   }
 }
